75cbb5473887489764a13971bfdf5512eacef04b
kevvor <keanov@gmail.com>
2017-06-29 15:53:54 +0000

    added basic sorting js to advanced sorting js

--- advanced-sorting.js
+++ advanced-sorting.js
@@ -1,3 +1,15 @@
+//basic sorting
+
+var array = [10, 2, 5, 1, 9];
+
+array.sort(function(a, b) {
+  return a - b;
+})
+
+console.log(array);
+
+// advanced sorting
+
 var students = [
   { id: 1, name: "bruce",    age: 40 },
   { id: 2, name: "zoidberg", age: 22 },
